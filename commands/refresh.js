06fd4c836714725eb3ae0408af4a17196ac7584b
Victor <victorvpn13@gmail.com>
2021-02-20 14:11:17 -0300

    adiciona atualização do ranking do atcoder ao dar o comando -refresh

--- commands/refresh.js
+++ commands/refresh.js
@@ -1,73 +1,57 @@
 const config = require("../config.json");
 
+const getCodeforcesData = require("../scripts/getCodeforcesData");
+const getAtcoderData = require("../scripts/getAtcoderData");
+const getAtcoderRankName = require("../scripts/getAtcoderRankName");
+
 module.exports = async ( client, msg ) => {
-    const ranks = config.ranks;
-    
+    const codeforces_ranks = config.codeforces_ranks;
+    const atcoder_ranks = config.atcoder_ranks;
+
     const serverRoles = msg.guild.roles.cache;
-
-    /*await user_codeforces.getRank("Tilnoene")
-        .then(rank => {
-            console.log(rank);
-        });*/
-
     const member = msg.member;
     const nickname = msg.guild.member(msg.author).nickname;
     const handle = nickname ? nickname : msg.author.username;
 
-    const https = require('https');
-    const options = {
-        hostname: config.api_codeforces,
-        path: `/api/user.info?handles=${handle}`,
-        method: 'GET'
-    }
-    
-    let data = "";
-    const req = await https.request(options, (res) => {
-        if(config.debug) console.log(`statusCode: ${res.statusCode}`);
-    
-        res.on("data", d => {
-            data += d;
-        });
-    
-        res.on("end", () => {
-            data = JSON.parse(data);
-            let rank = "newbie";
-
-            if(data.status == "OK") {
-                rank = data.result[0].rank;
-            } else {
-                console.log("Esse user não existe.");
-            }
-
-            rank = rank.toUpperCase();
-
-            if (config.debug) {
-                console.log(rank);
-                console.log(handle);
-            }
-
+    // Codeforces rank
+    await getCodeforcesData(handle)
+        .then(response => response.result[0].rank.toUpperCase())
+        .then(rank => {
             const role = serverRoles.find((role) => {
                 return role.name.toUpperCase() === rank
             });
-            
+
+            // Remove actual roles and add the new role
             if(role) {
-                member.roles._roles.map((atualRole) => {
-                    if (ranks.includes(atualRole.name.toLowerCase()) && atualRole !== role) {
-                        member.roles.remove(atualRole);
+                member.roles._roles.map((currentRole) => {
+                    if (codeforces_ranks.includes(currentRole.name.toLowerCase()) && currentRole !== role) {
+                        member.roles.remove(currentRole);
                     }
                 })
                 
                 member.roles.add(role);
-
-                msg.reply(`seu ranking foi atualizado com sucesso!`);
             }
-
         });
-    })
-
-    req.on('error', (error) => {
-        console.error(error);
-    });
     
-    req.end();
+    // AtCoder rank
+    await getAtcoderData(handle)
+        .then(rating => getAtcoderRankName(rating).toUpperCase())
+        .then(rank => {
+            const role = serverRoles.find((role) => {
+                return role.name.toUpperCase() === rank
+            });
+
+            // Remove actual roles and add the new role
+            if(role) {
+                member.roles._roles.map((currentRole) => {
+                    if (atcoder_ranks.includes(currentRole.name.toLowerCase()) && currentRole !== role) {
+                        member.roles.remove(currentRole);
+                    }
+                })
+                
+                member.roles.add(role);
+            }
+        });
+    
+    msg.reply(`seu ranking foi currentizado com sucesso!`);
 }
