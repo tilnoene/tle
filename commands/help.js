736e22c7be544ea4c06f7fd356b3db0569e7f144
Victor <victorvpn13@gmail.com>
2021-02-20 14:17:26 -0300

    modifica o comando help e remove o comando de avisos

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
-const config = require("../config.json");
 const commands = require("../scripts/commandsReader")(process.env.PREFIX);
 
 const descriptions = {
     "-help": "Obter a lista de comandos e informações sobre o bot",
-    "-refresh": "Atualiza seu cargo de acordo com seu rank atual do Codeforces",
+    "-refresh": "Atualiza seu cargo de acordo com seu rank atual do Codeforces e do AtCoder",
     "-rating": "Informa o rating e o rank do usuário informado"
 };
 
@@ -16,7 +15,7 @@ module.exports = async ( client, msg ) => {
         texto += `\n${"`" + command}${command === "-rating" ? " handle`" : "`"}: ${descriptions[command] ? descriptions[command] : "Sem descrição"}`;
     });
 
-    //texto += `\n\nLink do projeto: ${"https://www.google.com.br/"}`;
+    texto += `\n\nLink do projeto: ${"https://github.com/tilnoene/TLE"}`;
 
     msg.channel.send(texto);
 };
