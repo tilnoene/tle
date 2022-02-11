56466058843fc46e2a72e3b4294bbabf0956c3e8
Victor Manuel <victorvpn13@gmail.com>
2022-02-11 18:15:58 -0300

    fix: Adiciona IDs como variáveis de ambiente

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('./config.json');
 
 require('dotenv').config();
 
@@ -15,6 +14,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
